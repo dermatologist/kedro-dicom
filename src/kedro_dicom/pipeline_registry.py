2d5047be690da939421f218896706d05996adb50
Bell Eapen <github@gulfdoctor.net>
2022-05-13 16:35:11 -0500

    update registry

--- src/kedro_dicom/pipeline_registry.py
+++ src/kedro_dicom/pipeline_registry.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline, pipeline
 
+from kedro_dicom.pipelines.preprocess import pipeline as de
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -10,4 +11,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": pipeline([])}
+    de_pipeline = de.create_pipeline()
+
+    return {
+        "de": de_pipeline,
+        "__default__": de_pipeline,
+    }
